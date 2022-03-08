2e331fc66634d72517124481d0eff8e736c681e1
yunmee0704 <900704630c@gmail.com>
2022-03-08 21:47:33 +0900

    final

---
+++ src/pages/Write.js
@@ -0,0 +1,67 @@
+import Navbar from '../components/shared/Navbar'
+import Title from '../components/shared/title'
+import EmotionCheckbox from '../components/write/EmotionCheckBox'
+import TextArea from '../components/write/TextArea'
+import SubmitButton from '../components/write/SubmitButton'
+import styled from 'styled-components'
+import { useState } from 'react'
+import getDateNow from '../utils/getDateNow'
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const WritePage = () => {
+  const { nowDate } = getDateNow()
+  const [diary, setdiary] = useState({
+    id: Date.now(),
+    memo: '',
+    emotion: '',
+    createdAt: nowDate,
+  })
+
+  const handleDiaryMemo = (event) => {
+    setdiary({
+      ...diary,
+      memo: event.target.value,
+    })
+  }
+
+  const handlerEmotionClick = (emotion) => {
+    setdiary({
+      ...diary,
+      emotion,
+    })
+  }
+  const handleSubmit = () => {
+    const diaries = JSON.parse(
+      window.localStorage.getItem('DIARIES_KEY') || '[]',
+    )
+    // window.localStorage.setItem('DIARIES_KEY', JSON.stringify([]))
+
+    const newDiaries = [...diaries, diary]
+    window.localStorage.setItem('DIARIES_KEY', JSON.stringify(newDiaries))
+
+    window.history.back()
+  }
+
+  return (
+    <div>
+      <Container>
+        <Navbar />
+        <Title
+          title={`오늘은 \n 어떤 하루를 보내셨나요?`}
+          subtitle="TODO 오늘 날짜 해주기"
+        />
+        <EmotionCheckbox onClick={handlerEmotionClick} />
+        <TextArea label="기록" onChange={handleDiaryMemo} value={diary.memo} />
+        <SubmitButton label="작성하기" onClick={handleSubmit} />
+      </Container>
+    </div>
+  )
+}
+
+export default WritePage
